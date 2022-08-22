8472fc668803e1e7a37d4e99268876b10f2e1567
the lissimus <thelissimus@tuta.io>
2022-08-23 01:30:07 +0500

    Implemented raw mode
    Added makefile

---
+++ Makefile
@@ -0,0 +1,37 @@
+SRC = ted.c
+OBJ = $(SRC:.c=.o)
+
+VERSION = 0.1
+
+PREFIX = /usr/local
+
+CFLAGS = -std=c99 -Wall -Wextra -pedantic -O2
+CC = cc
+
+all: ted
+
+%.o: %.c
+	$(CC) -o $@ $< -c $(CFLAGS)
+
+ted: $(OBJ)
+	$(CC) -o $@ $(OBJ)
+
+clean:
+	rm -f ted $(OBJ) ted-$(VERSION).tar.gz
+
+dist: clean
+	mkdir -p ted-$(VERSION)
+	cp -r Makefile $(SRC) ted-$(VERSION)
+	tar -cf ted-$(VERSION).tar ted-$(VERSION)
+	gzip ted-$(VERSION).tar
+	rm -rf ted-$(VERSION)
+
+install: all
+	mkdir -p $(DESTDIR)$(PREFIX)/bin
+	cp -f ted $(DESTDIR)$(PREFIX)/bin
+	chmod 755 $(DESTDIR)$(PREFIX)/bin/ted
+
+uninstall:
+	rm -f $(DESTDIR)$(PREFIX)/bin/ted
+
+.PHONY: all clean dist install uninstall
---
+++ ted.c
@@ -0,0 +1,71 @@
+#include <ctype.h>
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <termios.h>
+#include <unistd.h>
+
+struct termios orig_termios;
+
+void enable_raw_mode(void);
+void disable_raw_mode(void);
+void die(const char *);
+
+int main(void)
+{
+	enable_raw_mode();
+
+	for (;;) {
+		char c = '\0';
+		if (read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN) {
+			die("read");
+		}
+
+		if (iscntrl(c)) {
+			printf("%d\r\n", c);
+		} else {
+			printf("%d ('%c')\r\n", c, c);
+		}
+
+		if (c == 'q') {
+			break;
+		}
+	}
+
+	return 0;
+}
+
+void enable_raw_mode(void)
+{
+	struct termios raw;
+
+	if (tcgetattr(STDIN_FILENO, &orig_termios) == -1) {
+		die("tcgetattr");
+	}
+	atexit(disable_raw_mode);
+
+	raw = orig_termios;
+	raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
+	raw.c_oflag &= ~(OPOST);
+	raw.c_cflag |= (CS8);
+	raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
+	raw.c_cc[VMIN] = 0;
+	raw.c_cc[VTIME] = 1;
+
+	if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1) {
+		die("tcsetattr");
+	}
+}
+
+void disable_raw_mode(void)
+{
+	if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) == -1) {
+		die("tcsetattr");
+	}
+}
+
+void die(const char *s)
+{
+	perror(s);
+	exit(1);
+}
